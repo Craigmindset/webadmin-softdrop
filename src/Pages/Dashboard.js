ac9749a01c47f3d262e3de3790a73eecf5dfbca3
Henry Okeke <iamhenryokeke@gmail.com>
2023-09-03 14:57:28 +0100

    implememted dashbaord cards section

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,12 +1,16 @@
-import { Link } from "react-router-dom";
 import DashHeader from "../components/DashHeader";
+import DashboardCard from "../components/DashboardCard";
+import Naira from "../assets/Naira";
 
 export default function Dashboard(){
     return(
-        <div>
+        <main className="p-6">
             <DashHeader title={'Dashboard'} />
-            <div>This is the dashboard</div>
-            <Link to={"/admin/senders"}>go to senders</Link>
-        </div>
+            <section className="grid grid-cols-3 gap-6">
+                <DashboardCard title="Total Senders" percentage={2.5} amount="10,567"/>
+                <DashboardCard title="Total Travelers" percentage={-4.4} amount="8,245"/>
+                <DashboardCard title="Total Income" percentage={2.5} naira={<Naira/>} amount="45,240,750,000"/>
+            </section>
+        </main>
     )
 }
